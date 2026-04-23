532c1f2a6c7f8f3d4478c46fe6b660d575292a76
dipingit <diproyf87@gmail.com>
2026-04-23 00:01:28 -0300

    (server): added updateExpense and deleteExpense api

--- server/src/controller/expense.controller.ts
+++ server/src/controller/expense.controller.ts
@@ -75,3 +75,61 @@ export const getExpenseByID = async(req: Request, res: Response) => {
     }
 }
 
+//update expense
+export const updateExpense = async(req: Request, res: Response) => {
+    try {
+        const {id} = req.params;
+        const userId = req.userId;
+        if(!userId) return res.status(401).json({error: "Unauthorized"});
+        const {amount, description, categoryId} = req.body;
+        
+        // Update the expense
+        const updatedExpense = await prisma.expense.update({
+            where: { id: parseInt(id), userId: userId },
+            data: {
+                amount: amount ? parseFloat(amount) : undefined,
+                description: description || null,
+                categoryId
+            },
+            include: {
+                category: true
+            }
+        });
+        
+        res.status(200).json({message: "Expense updated successfully", data: updatedExpense});
+    } catch (error: any) {
+        if(error.code === 'P2003'){
+            return res.status(404).json({error: 'Category not found'});
+        }
+        if(error.code === 'P2025'){
+            return res.status(404).json({error: 'Expense not found'});
+        }
+        res.status(500).json({error: error.message});
+    }
+}
+
+//delete expense
+export const deleteExpense = async(req: Request, res: Response) => {
+    try{
+        const { id } = req.params;
+        const userId = req.userId; 
+        if(!userId) return res.status(401).json({error: "Unauthorized"});
+        
+        //check if expense exist
+        const isExpenseExist = await prisma.expense.findFirst({
+            where: {id: parseInt(id), userId: userId}
+        });
+        if(!isExpenseExist) return res.status(404).json({error: "expense not found"});
+        
+        await prisma.expense.delete({
+            where: { id: parseInt(id), userId: userId }
+        });
+        
+        res.status(200).json({message: "Expense deleted successfully"});
+    }catch(error: any){
+        if(error.code === 'P2025'){
+            return res.status(404).json({error: "Expense not found"});
+        }
+        res.status(500).json({error: error.message});
+    }
+}
--- server/src/router/expense.routes.ts
+++ server/src/router/expense.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createExpense, getAllExpenses, getExpenseByID } from '../controller/expense.controller';
+import { createExpense, deleteExpense, getAllExpenses, getExpenseByID, updateExpense } from '../controller/expense.controller';
 import { validateRequest } from '../middleware/validation.middleware';
 import { createExpenseSchema, getExpenseByIDSchema } from '../utils/validation.schemas';
 import { protect, validateBearerFormat } from '../middleware/auth.middleware';
@@ -12,5 +12,7 @@ const router = express.Router();
 router.post('/expenses', validateBearerFormat, protect, validateRequest(createExpenseSchema), createExpense);
 router.get('/expenses', validateBearerFormat, protect, getAllExpenses);
 router.get('/expenses/:id', validateBearerFormat, protect, validateRequest(getExpenseByIDSchema), getExpenseByID);
+router.patch('/expenses/:id', validateBearerFormat, protect, validateRequest(createExpenseSchema), updateExpense);
+router.delete('/expenses/:id', validateBearerFormat, protect, deleteExpense);
 
 export default router;
