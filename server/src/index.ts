ed294b4015a952d244a34b5ce0a2c8c2656b27d6
dipingit <diproyf87@gmail.com>
2026-05-17 02:44:34 -0300

    (server): modified express app PORT

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use('/api', expenseRoutes);
 app.use('/api', categoryRoutes);
